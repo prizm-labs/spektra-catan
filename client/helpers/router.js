0e85c107cefe86af2455553ee8dc1d022a4a47d8
Michael Garrido <michael.a.garrido@gmail.com>
2014-08-08 05:58:50 -0700

    remove wrong Crafty render init

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -57,9 +57,7 @@ Router.map(function() {
 
         },
         onRun: function(){
-            Crafty.init(500,350, document.getElementById('game'));
 
-            Crafty.e('2D, Canvas, Color').attr({x: 0, y: 0, w: 100, h: 100}).color('#F00');
         }
     });
 
