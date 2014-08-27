1bc34cc0dce387409761a36a32c3683d5ccd1c70
Michael Garrido <michael.a.garrido@gmail.com>
2014-08-26 23:13:50 -0700

    2D bodies with variants (i.e terrain, type)

--- client/helpers/mobileHelpers.js
+++ client/helpers/mobileHelpers.js
@@ -2,20 +2,7 @@
 Template.handView.rendered = function() {
     console.log('hand view rendered');
 
-
-    ctx = new PRIZM.Context2D('handContext', 'canvas', 800, 800);
-    ctx.init();
-
-
-    ctx3D = new PRIZM.Context3D('fieldContext', 800, 800);
-    ctx3D.init();
-
     dep = new Deps.Dependency;
-
-
-
-
-    factory = new PRIZM.Factory();
 
     manifest3D = [
         ['road', 'models/road-model.js', null, 0.5],
@@ -23,19 +10,72 @@ Template.handView.rendered = function() {
         ['city','models/city-model.js',null, 0.5]
     ];
 
+    manifest2D = [
+        ["robber","robber.png"],
+        ["numberToken",{
+            2: "count-2.png",
+            3: "count-3.png",
+            4: "count-4.png",
+            5: "count-5.png",
+            6: "count-6.png",
+            7: "count-7.png",
+            8: "count-8.png",
+            9: "count-9.png",
+            10: "count-10.png",
+            11: "count-11.png",
+            12: "count-12.png"
+        }],
+        ["terrain",{
+            "mountains": "terrain-mountains.png",
+            "hills": "terrain-hills.png",
+            "forest": "terrain-forest.png",
+            "pasture": "terrain-pasture.png",
+            "desert": "terrain-desert.png",
+            "fields": "terrain-fields.png"
+        }],
+        ["resourceCard",{
+            "back": "resource-back.png",
+            "ore": "resource-ore.png",
+            "sheep": "resource-sheep.png",
+            "grain": "resource-wheat.png",
+            "lumber": "resource-wood.png",
+            "brick": "resource-brick.png"
+        }],
+        ["developmentCard",{
+            "back": "dev-back.png",
+            "monopoly": "dev-monopoly.png",
+            "palace": "dev-palace.png",
+            "knight": "dev-knight.png",
+            "yearOfPlenty": "dev-plenty.png",
+            "roadBuilding": "dev-road.png",
+            "market": "dev-market.png",
+            "library": "dev-library.png",
+            "university": "dev-university.png"
+        }],
+        ["port",{
+            "grain": "port-grain.png",
+            "lumber": "port-lumber.png",
+            "brick": "port-brick.png",
+            "ore": "port-ore.png",
+            "wool": "port-wool.png",
+            "any": "port-any.png"
+        }]
+    ];
+
+    ctx2D = new PRIZM.Context2D('handContext', 'canvas', 800, 800);
+    ctx2D.init();
+
+    ctx3D = new PRIZM.Context3D('fieldContext', 800, 800);
+    ctx3D.init();
+
+    factory = new PRIZM.Factory( ctx2D, ctx3D );
     factory.loadTemplates3D(manifest3D);
+    factory.loadTemplates2D( 'atlas/atlas.json', manifest2D );
 
     //TODO wait for load complete, before creating objects
 
-
-    b1 = factory.makeBody( 100, 100 );
-
-
+    //b1 = factory.makeBody2D( 100, 100, 'terrain', { variant: 'pasture' } );
     //b2 = factory.makeBody3D( 'road', 1,1,1);
-
-
-
-
 
 
 };
