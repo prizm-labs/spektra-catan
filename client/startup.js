78847e5beb298135f0d3ba41b603f29dc905c11d
Michael Garrido <michael.a.garrido@gmail.com>
2014-09-02 01:50:33 -0700

    refactor LiveData classes

--- client/startup.js
+++ client/startup.js
@@ -64,7 +64,7 @@ Meteor.startup(function () {
 
             if (mainView) {
 
-                liveDataDelegate = new LiveDataDelegate();
+                liveDataDelegate = new PRIZM.LiveData();
 
                 nodeHandle = Meteor.subscribe('nodes',function onReady(){
                     console.log('subscribed to public nodes !!!');
@@ -99,36 +99,3 @@ Meteor.startup(function () {
     })
 
 });
-
-function LiveDataDelegate(){
-
-    // bind collection (or document) to reactive object
-    //i.e. Node document on server to Body2D on client
-
-    // with a translate function: position in 2D context
-
-    this.subscriptions = {};
-}
-
-LiveDataDelegate.prototype = {
-
-    registerSubscription: function( collection, id, translation ){
-
-        this.subscriptions[collection+id] = translation;
-
-    },
-
-    updateSubscriptions: function( ddpMsg ){
-
-        console.log('updateSubscriptions',ddpMsg);
-        //if id matches subscription, call translation
-        // ex:  { "msg":"changed",
-        // "collection":"nodes",
-        // "id":"qwiyKk5SFwZG9E4ca",
-        // "fields":{"x":200,"y":200}}
-
-        if (this.subscriptions[ddpMsg.collection+ddpMsg.id]) this.subscriptions[ddpMsg.collection+ddpMsg.id](ddpMsg.fields);
-
-    }
-
-}
